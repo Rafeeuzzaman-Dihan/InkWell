ccdbc166bc14321b09585315fcddfaa0a8e7ebf9
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-03-03 15:49:23 +0600

    User Dashboard Updated

--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -1,4 +1,3 @@
 <section class="bg-white shadow-md rounded-lg p-4 mb-8">
-    <h2 class="text-xl font-semibold mb-4">User Features</h2>
-    <p>User Dashboard</p>
+    <h2 class="text-xl font-semibold mb-4">User Dashboard</h2>
 </section>
