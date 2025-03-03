ea631ff8a844af9e6645aaa43fb0f1f9a38cac3b
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-03-03 14:47:56 +0600

    Middleware added for routes

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,16 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
-
-Route::post('/categories', [CategoryController::class, 'store'])->middleware('auth')->name('categories.store');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('likes.store');
-Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('likes.destroy');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('likes.store');
+    Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('likes.destroy');
+});
