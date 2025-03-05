9ad69bfebcf5484378dd23bc7799347abd3a7547
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-03-05 09:11:59 +0600

    User Updating Successfully - Admin Panel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -16,6 +17,21 @@ class UserController extends Controller
     {
         $user = User::findOrFail($id);
         return view('admin.edit_user', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $id,
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully');
     }
 
     public function destroy($id)
