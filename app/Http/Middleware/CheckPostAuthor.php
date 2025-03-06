d13f17d5b54b96453f94bb46ce1fce7b80f70a52
Rafeeuzzman-Dihan <rafeeuzzamandihan@gmail.com>
2025-03-06 10:43:38 +0600

    Route Fix

--- app/Http/Middleware/CheckPostAuthor.php
+++ app/Http/Middleware/CheckPostAuthor.php
@@ -11,15 +11,12 @@ class CheckPostAuthor
 {
     public function handle(Request $request, Closure $next)
     {
-        // Find the post by ID from the route
         $post = Post::findOrFail($request->route('post'));
 
-        // Allow admin to access any post or the author to access their own post
         if (Auth::user()->is_admin || $post->user_id === Auth::id()) {
-            return $next($request); // Proceed to the next request handler
+            return $next($request);
         }
 
-        // Redirect if the user does not have permission
         return redirect()->route('dashboard')->with('error', 'You do not have permission to manage this post.');
     }
 }
